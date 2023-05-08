aa7efa11ee548543548b17a92f060599ab343595
d22hanol <d22hanol@student.his.se>
2023-05-08 15:18:23 +0200

    Created a sharedPreferences and a means to edit it in preparation for display

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,13 +3,19 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button goBack;
+
+    private Button save;
+
+    private EditText inputData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +23,26 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         goBack = (Button) findViewById(R.id.button2);
 
+        inputData = (EditText) findViewById(R.id.data);
+
+        final String sharedString = inputData.getText().toString();
+
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SecondActivity.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        save = (Button) findViewById(R.id.savebutton);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences saveFile = getSharedPreferences("sharekey", MODE_PRIVATE);
+                SharedPreferences.Editor saveEditor = saveFile.edit();
+                saveEditor.putString("sharekey",sharedString);
             }
         });
     }
